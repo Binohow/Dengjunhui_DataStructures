4ec9d5445d9f86533296a67018e36cc6c6af6f92
howbin <howbin13191201638@outlook.com>
2020-06-27 15:49:25 +0800

    代码1.2 整数事迕刢展开中数位1总数癿统计

--- 1_2.cpp
+++ 1_2.cpp
@@ -2,6 +2,8 @@
  * 统计整数n癿二迕刢展开中数位1癿总数： O(logn)
 */
 #include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
 int countOnes(unsigned int n)
 {
     int ones=0;
@@ -10,10 +12,10 @@ int countOnes(unsigned int n)
         ones += (1&n);  // 位运算巧妙
         n >>= 1;
     }
-    return 
+    return ones;
 }
 int main()
 {
-    
+    cout<<countOnes(7);
     return 0;
 }
